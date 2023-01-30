90987444ee34d2a6da2a840f187de68fd4ddb0e3
Pavel <p@kkmspb.ru>
2023-01-30 19:54:16 +0300

    7.1.0.0

--- pbltableview_test/version.h
+++ pbltableview_test/version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROG_VERSION    "8.0.0.1"
-#define VER1    8
-#define VER2    0
+#define PROG_VERSION    "7.1.0.0"
+#define VER1    7
+#define VER2    1
 #define VER3    0
-#define VER4    1
+#define VER4    0
 
 #endif // VERSION_H
