59bb5b6ba7189053c6ed8b33bbb43f6ecc90c932
Pavel <p@kkmspb.ru>
2023-01-30 19:58:50 +0300

    8.0.0.2 beta
    add second constructor (protected) to PblTableDlg class for convenient inheritance in future.

--- pbltableview_test/version.h
+++ pbltableview_test/version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROG_VERSION    "7.1.0.0"
-#define VER1    7
-#define VER2    1
+#define PROG_VERSION    "8.0.0.2"
+#define VER1    8
+#define VER2    0
 #define VER3    0
-#define VER4    0
+#define VER4    2
 
 #endif // VERSION_H
